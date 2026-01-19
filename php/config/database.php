d3529a921aff190537a657fd0e2f9718aef1718c
omar95ghass <omar.ghass95@gmail.com>
2026-01-19 15:43:17 +0300

    Change database password to empty string
    Updated database password to an empty string.

--- php/config/database.php
+++ php/config/database.php
@@ -3,6 +3,6 @@ return [
     'host' => 'mysql-omar95sy.alwaysdata.net',
     'dbname' => 'omar95sy_cmms',
     'username' => 'omar95sy',
-    'password' => 'Blue.dress995',
+    'password' => '',
     'charset' => 'utf8mb4',
 ];
