9fa511a76af49630dc5985c1322f48717d916da3
omar95ghass <omar.ghass95@gmail.com>
2026-01-19 15:48:44 +0300

    Clear database connection settings
    Updated database configuration to use empty values.

--- php/config/database.php
+++ php/config/database.php
@@ -1,8 +1,8 @@
 <?php
 return [
-    'host' => 'mysql-omar95sy.alwaysdata.net',
-    'dbname' => 'omar95sy_cmms',
-    'username' => 'omar95sy',
+    'host' => '',
+    'dbname' => '',
+    'username' => '',
     'password' => '',
     'charset' => 'utf8mb4',
 ];
